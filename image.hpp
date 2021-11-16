24be27b5d1fd22b8fc1473863ebcf2b09104204d
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-16 14:49:32 +0530

    Create image.hpp

---
+++ image.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+typedef struct node{
+	bool is_leaf;
+	int pixel_gray_value;
+	int * p;
+	int top;
+	int left;
+	int bottom;
+	int right;
+	int difference;
+	
+       int prows;
+       int pcols;
+       
+	node* north_west;
+    	node* north_east;
+    	node* south_east;
+    	node* south_west;
+}node;
+
+node* new_node();
+void add_values(node* q, int* p, int num_rows, int num_cols);
+void split_node(node* q);
+
+float avg_pxvalue(node* q);
+float pixel_variance(node* q);
+void build_quad_tree(node* root, int var);
+void_make_matrix(node*start, cv::Mat& modified, int total_rows, int total_cols);
+int count_tree_nodes(node* root);
