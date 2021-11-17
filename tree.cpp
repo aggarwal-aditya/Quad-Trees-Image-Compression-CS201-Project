f983a5d66d9fecb1efd8e9e686dbfa4335ae3d8b
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-17 07:19:13 +0530

    Bug fixes

--- tree.cpp
+++ tree.cpp
@@ -1,20 +1,20 @@
 //
 // Created by Aditya Aggarwal on 22-10-2021.
 //
+
 #ifndef test
 #define test
+#include "Tree.h"
 
-int absolute_value(int x) {
+int absolute_value_fun(int x) {
     if (x < 0)
         x = -x;
     return x;
 }
 
-#include "Tree.h"
-
 Quad::Quad() {
-    topLeft = coordinates(0, 0);
-    botRight = coordinates(0, 0);
+    botLeft = coordinates(0, 0);
+    topRight = coordinates(0, 0);
     n = nullptr;
     topLeftTree = nullptr;
     topRightTree = nullptr;
@@ -22,14 +22,14 @@ Quad::Quad() {
     botRightTree = nullptr;
 }
 
-Quad::Quad(coordinates topL, coordinates botR) {
+Quad::Quad(coordinates botL, coordinates topR) {
     n = nullptr;
     topLeftTree = nullptr;
     topRightTree = nullptr;
     botLeftTree = nullptr;
     botRightTree = nullptr;
-    topLeft = topL;
-    botRight = botR;
+    botLeft = botL;
+    topRight = topR;
 }
 
 void Quad::insert(Node *_node) {
@@ -37,33 +37,33 @@ void Quad::insert(Node *_node) {
         return;
     if (!inboundary(_node->loc))
         return;
-    if (absolute_value(topLeft.x - botRight.x) <= 1 && absolute_value((topLeft.x - botRight.y) <= 1)) {
+    if (absolute_value_fun(botLeft.x - topRight.x) <= 1 && (absolute_value_fun(botLeft.x - topRight.y) <= 1)) {
         if (n == nullptr)
             n = _node;
         return;
     }
-    if ((topLeft.x + botRight.x) / 2 >= _node->loc.x) {
+    if ((botLeft.x + topRight.x) / 2 >= _node->loc.x) {
         if (topLeftTree == nullptr) {
-            topLeftTree = new Quad(coordinates(topLeft.x, topLeft.y),
-                                   coordinates((topLeft.x + botRight.x) / 2, ((topLeft.y + botRight.y) / 2)));
+            topLeftTree = new Quad(coordinates(botLeft.x, botLeft.y),
+                                   coordinates((botLeft.x + topRight.x) / 2, ((botLeft.y + topRight.y) / 2)));
             topLeftTree->insert(_node);
         } else {
             if (botLeftTree == nullptr)
-                botLeftTree = new Quad(coordinates(topLeft.x, (topLeft.y + botRight.y) / 2),
-                                       coordinates((topLeft.x + botRight.x) / 2, botRight.y));
+                botLeftTree = new Quad(coordinates(botLeft.x, (botLeft.y + topRight.y) / 2),
+                                       coordinates((botLeft.x + topRight.x) / 2, topRight.y));
             botLeftTree->insert(_node);
         }
     } else {
-        if ((topLeft.y + botRight.y) / 2 >= _node->loc.y) {
+        if ((botLeft.y + topRight.y) / 2 >= _node->loc.y) {
             if (topRightTree == nullptr)
-                topRightTree = new Quad(coordinates((topLeft.x + botRight.x) / 2, topLeft.y),
-                                        coordinates(botRight.x, (topLeft.y + botRight.y) / 2));
+                topRightTree = new Quad(coordinates((botLeft.x + topRight.x) / 2, botLeft.y),
+                                        coordinates(topRight.x, (botLeft.y + topRight.y) / 2));
             topRightTree->insert(_node);
         } else {
             if (botRightTree == nullptr)
                 botRightTree = new Quad(
-                        coordinates((topLeft.x + botRight.x) / 2, (topLeft.y + botRight.y) / 2),
-                        coordinates(botRight.x, botRight.y));
+                        coordinates((botLeft.x + topRight.x) / 2, (botLeft.y + topRight.y) / 2),
+                        coordinates(topRight.x, topRight.y));
             botRightTree->insert(_node);
         }
     }
@@ -74,8 +74,8 @@ Node *Quad::search(coordinates point) {
         return nullptr;
     if (n != nullptr)
         return n;
-    if ((topLeft.x + botRight.x) / 2 >= point.x) {
-        if ((topLeft.x + botRight.y) / 2 >= point.y) {
+    if ((botLeft.x + topRight.x) / 2 >= point.x) {
+        if ((botLeft.x + topRight.y) / 2 >= point.y) {
             if (topLeftTree == nullptr)
                 return nullptr;
             return topLeftTree->search(point);
@@ -85,7 +85,7 @@ Node *Quad::search(coordinates point) {
             return botLeftTree->search(point);
         }
     } else {
-        if ((topLeft.y + botRight.y) / 2 >= point.y) {
+        if ((botLeft.y + topRight.y) / 2 >= point.y) {
             if (topRightTree == nullptr)
                 return nullptr;
             return topRightTree->search(point);
@@ -98,7 +98,7 @@ Node *Quad::search(coordinates point) {
 }
 
 bool Quad::inboundary(coordinates point) {
-    return (point.x >= topLeft.x && point.x <= botRight.x && point.y >= topLeft.y && point.y <= botRight.y);
+    return (point.x >= botLeft.x && point.x <= topRight.x && point.y >= botLeft.y && point.y <= topRight.y);
 }
 
 #endif
