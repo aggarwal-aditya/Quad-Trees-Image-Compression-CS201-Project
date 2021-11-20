85a28a2ec6e80e1803e007bc655aee164260955f
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-20 20:35:55 +0530

    Updated to Look like professional code
    Ask image path and compression value

--- img_proc/main.cpp
+++ img_proc/main.cpp
@@ -7,18 +7,34 @@ using namespace std;
 int main()
 {
     int max_comp=100;
-    Mat original = imread("index.png", IMREAD_GRAYSCALE);
-    Mat modified = imread("index.png", IMREAD_GRAYSCALE);
-    if (original.empty()){
-        cout << "Could not open or find image"<< endl;
-        return -1;
+    string s; 
+    cout<<"Please Enter the path of Image\n";
+    cin>>s;
+    Mat original = imread(s, IMREAD_GRAYSCALE);
+    Mat modified = imread(s, IMREAD_GRAYSCALE);
+    while(original.empty()){
+    	if (original.empty()){
+        cout << "No such Image found,please try again"<< endl;
+    	}
+    	cin>>s;
+    	 original = imread(s, IMREAD_GRAYSCALE);
+    	 modified = imread(s, IMREAD_GRAYSCALE);
+    
     }
+    cout<<"If you want to set maximum image variance of compressed image (a measure for compression) press Y, else press any other key to continuw with default setting"<<endl;
+    char ch; cin>>ch;
+    if(ch=='Y')
+    {
+    	cout<<endl<<"Please enter the value of image variance";
+    	cin>>max_comp;
+    }
+    
     int rows=modified.rows;
     int cols=modified.cols;
     
     cout << "Rows: " <<  rows << endl;
     cout << "Cols: " <<  cols << endl;
-    cout << "Total pixels: " << rows * cols << endl;
+    cout << "Total pixels in orignal Image: " << rows * cols << endl;
     
     int* pixels = new int[rows*cols];
     
@@ -31,9 +47,10 @@ int main()
     node * q = new_node();
     add_values(q, pixels, rows, cols);
     build_quad_tree(q,max_comp);
-    cout << "Nodes: " << new_pixels_count(q) << endl;
+    cout << "Nodes in Quad Tree - An approximate for new pixels: " << new_pixels_count(q) << endl;
     make_matrix(q, modified, rows, cols);
-    imshow("Original", modified);
+    imshow("Original", original);
+    imshow("Compressed", modified);
     imwrite("Modified.jpeg", modified);
     waitKey(0);
     return 0;
