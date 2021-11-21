024fc6f23d42e170c0085c0cd36ff3b85f254775
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-21 17:18:09 +0530

    Update main.cpp

--- img_proc/main.cpp
+++ img_proc/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <opencv2/opencv.hpp> 
+#include <opencv2/opencv.hpp> //Include OpenCV Library
 #include "image.hpp"
 using namespace cv;
 using namespace std;
 
 int main()
 {
-    int max_comp=100;
+    int max_comp=100;//MAX Variance of image
     string s; 
     cout<<"Please Enter the path of Image\n";
     cin>>s;
-    Mat original = imread(s, IMREAD_GRAYSCALE);
-    Mat modified = imread(s, IMREAD_GRAYSCALE);
+    Mat original = imread(s, IMREAD_GRAYSCALE);//Read Original Image
+    Mat modified = imread(s, IMREAD_GRAYSCALE);//Initialize an empty matrix for modified image with original image to get the same size
     while(original.empty()){
     	if (original.empty()){
         cout << "No such Image found,please try again"<< endl;
@@ -29,6 +29,7 @@ int main()
     	cin>>max_comp;
     }
     
+    //get count of rows and cols in matrix
     int rows=modified.rows;
     int cols=modified.cols;
     
@@ -36,19 +37,27 @@ int main()
     cout << "Cols: " <<  cols << endl;
     cout << "Total pixels in orignal Image: " << rows * cols << endl;
     
-    int* pixels = new int[rows*cols];
+    int* pixels = new int[rows*cols];//Make a new array to store all pixel values
     
+    //Add pixel values to the array from image
     for(int r = 0; r < rows; r++){
         for(int c = 0; c < cols; c++){
             pixels[r*cols+c]=modified.at<uint8_t>(r,c);
         }
     }
 
+    //Make a root for quad tree
     node * q = new_node();
+    
+    //We initalize the node with the pixel array number of rows and cols in image. As of now one node contains the entire information of image.
     add_values(q, pixels, rows, cols);
     build_quad_tree(q,max_comp);
+    
     cout << "Nodes in Quad Tree - An approximate for new pixels: " << new_pixels_count(q) << endl;
+    
+    //We make a CV matrix from the compressed image
     make_matrix(q, modified, rows, cols);
+    
     imshow("Original", original);
     imshow("Compressed", modified);
     imwrite("Modified.jpeg", modified);
