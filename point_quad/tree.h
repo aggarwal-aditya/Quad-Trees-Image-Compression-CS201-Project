e9e2c0cc93628a7e860e5ac2f5c89d2bc2b5a02b
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-21 16:38:37 +0530

    Added comments

--- point_quad/tree.h
+++ point_quad/tree.h
@@ -4,14 +4,20 @@
 #define QUAD_TREES_TREE_H
 class Quad
 {
-    coordinates botLeft;
-    coordinates topRight;
+    //Boundary Value of the node
+    coordinates botLeft;//x coordinate of left edge of square, y coordinate of bottom edge of square
+    coordinates topRight;//x coordinate of right edge of square, y coordinate of top edge of square
+    
+    //Contains details of this node
     Node*n;
+    
+    //The below four pointers are refferences to its four children
     Quad *topLeftTree;
     Quad *topRightTree;
     Quad *botLeftTree;
     Quad *botRightTree;
 public:
+    //We declare all functions of Quad class here and implement them in tree.cpp to beautify the code
     Quad();
     Quad(coordinates botL,coordinates topR);
     void insert(Node*);
